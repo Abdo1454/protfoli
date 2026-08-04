99b5860af79427b48cdcf7ef46b4e5dae05bdb4a
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-08-04 05:17:27 +0300

    Add responsive Bootstrap navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,10 +1,25 @@
+import React from "react";
+import logo from "../../assets/icons/icon-brand.png";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
       <div className="container">
-        <a className="navbar-brand fw-bold" href="#">
-          Abdulrahim
-        </a>
+        <h2>
+            <img src={logo} alt="Logo" />
+        </h2>
+
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
       </div>
     </nav>
   );
