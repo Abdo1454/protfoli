6a5eaaf3fc69332a82b2f36d6cbc0149cbc80aae
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-08-04 04:36:13 +0300

    Add responsive navbar component

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+      <div className="container">
+        <a className="navbar-brand fw-bold" href="#">
+          Abdulrahim
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
