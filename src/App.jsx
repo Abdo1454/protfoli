e21eec5db2f5ce8c86ebcb3c5f2de31ac02ab40d
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-08-05 14:29:04 +0300

    Add responsive skills section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Layout/Footer';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import './index.css';
+import Skills from './components/Skills/Skills';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <main className="bg-section">
         <Hero />
         <About />
+        <Skills />
       </main>
       <Footer />
     </div>
